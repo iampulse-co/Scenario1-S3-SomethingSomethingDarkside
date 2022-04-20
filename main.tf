8231e004c4fc57c0ea7d4dee06b35b420867f27a
Kyler Middleton <kyler.middleton@iampulse.com>
2022-04-20 15:23:17 -0500

    Update Developers group to PowerUsers

--- main.tf
+++ main.tf
@@ -93,6 +93,85 @@ resource "aws_iam_group_policy" "S3-AdminsPolicy" {
   })
 }
 
+resource "aws_iam_group" "S3-PowerUsers" {
+  name = "S3-PowerUsers"
+}
+
+resource "aws_iam_group_policy" "S3-PowerUsers" {
+  name  = "S3-PowerUsers-Policy"
+  group = aws_iam_group.S3-PowerUsers.name
+  # Before policy
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Id" : "S3PowerUsers",
+    "Statement" : [
+      {
+        "Sid" : "S3PowerUsers",
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:GetBucketAccelerateConfiguration",
+          "s3:GetBucketAcl",
+          "s3:GetBucketAnalyticsConfiguration",
+          "s3:GetBucketCors",
+          "s3:GetBucketEncryption",
+          "s3:GetBucketIntelligentTieringConfiguration",
+          "s3:GetBucketInventoryConfiguration",
+          "s3:GetBucketLifecycle",
+          "s3:GetBucketLifecycleConfiguration",
+          "s3:GetBucketLocation",
+          "s3:GetBucketLogging",
+          "s3:GetBucketMetricsConfiguration",
+          "s3:GetBucketNotification",
+          "s3:GetBucketNotificationConfiguration",
+          "s3:GetBucketOwnershipControls",
+          "s3:GetBucketPolicy",
+          "s3:GetBucketPolicyStatus",
+          "s3:GetBucketReplication",
+          "s3:GetBucketRequestPayment",
+          "s3:GetBucketTagging",
+          "s3:GetBucketVersioning",
+          "s3:GetBucketWebsite",
+          "s3:GetObject",
+          "s3:GetObjectAcl",
+          "s3:GetObjectAttributes",
+          "s3:GetObjectLegalHold",
+          "s3:GetObjectLockConfiguration",
+          "s3:GetObjectRetention",
+          "s3:GetObjectTagging",
+          "s3:GetObjectTorrent",
+          "s3:GetPublicAccessBlock",
+          "s3:ListBucketAnalyticsConfigurations",
+          "s3:ListBucketIntelligentTieringConfigurations",
+          "s3:ListBucketInventoryConfigurations",
+          "s3:ListBucketMetricsConfigurations",
+          "s3:ListBuckets",
+          "s3:ListMultipartUploads",
+          "s3:ListObjects",
+          "s3:ListObjectsV2",
+          "s3:ListObjectVersions",
+          "s3:ListParts"
+        ],
+        "Resource" : flatten(
+          concat(
+            [
+              for v in aws_s3_bucket.logging-s3-buckets : [
+                "arn:aws:s3:::${v.bucket}",
+                "arn:aws:s3:::${v.bucket}/*"
+              ]
+            ],
+            [
+              for v in aws_s3_bucket.data-s3-buckets : [
+                "arn:aws:s3:::${v.bucket}",
+                "arn:aws:s3:::${v.bucket}/*"
+              ]
+            ]
+          )
+        )
+      }
+    ]
+  })
+}
+
 resource "aws_iam_group_membership" "DevelopersGroupMembership" {
   name = "Developers-Group-Membership"
 
@@ -100,5 +179,5 @@ resource "aws_iam_group_membership" "DevelopersGroupMembership" {
     for v in aws_iam_user.developers_iam_users : v.name
   ]
 
-  group = aws_iam_group.S3-Admins.name
+  group = aws_iam_group.S3-PowerUsers.name
 }
